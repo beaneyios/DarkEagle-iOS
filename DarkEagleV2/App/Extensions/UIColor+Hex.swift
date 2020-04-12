fcc2620013fb662caeee62c889e4262583be37b1
Matt Beaney <beaneyios@gmail.com>
2020-04-12 18:58:18 +0100

    Tidied up hex init code.

--- DarkEagleV2/App/Extensions/UIColor+Hex.swift
+++ DarkEagleV2/App/Extensions/UIColor+Hex.swift
@@ -10,28 +10,29 @@ import UIKit
 
 extension UIColor {
     public convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
-
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-
-            if hexColor.count == 6 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
+        guard hex.hasPrefix("#") else {
+            return nil
         }
+        
+        let start = hex.index(hex.startIndex, offsetBy: 1)
+        let hexColor = String(hex[start...])
 
-        return nil
+        guard hexColor.count == 6 else {
+            return nil
+        }
+        
+        let scanner = Scanner(string: hexColor)
+        var hexNumber: UInt64 = 0
+
+        guard scanner.scanHexInt64(&hexNumber) else {
+            return nil
+        }
+        
+        let r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+        let g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+        let b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+        let a = CGFloat(hexNumber & 0x000000ff) / 255
+
+        self.init(red: r, green: g, blue: b, alpha: a)
     }
 }
